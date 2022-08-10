de1d56f99be91aa56a6787e6c1db17e0ee887634
Virginia224 <47594162+Virginia224@users.noreply.github.com>
2022-08-10 11:21:18 +0200

    description of code and instructions

--- NBspie.R
+++ NBspie.R
@@ -1,21 +1,26 @@
 NBspie <- function(outcomesBen, outcomesHarm, weightsBen, weightsHarm, labelsBen, labelsHarm, lambda = seq(0, 1, by=0.05)) {
   
   ### FUNCTION ARGUMENTS:
-  # outcomesBen = array of beneficial outcomes with treatments as row names; must be between 0-1
-  # outcomesHarm = array of harmful outcomes with treatments as row names; must be between 0-1
-  # weightsBen = (vector of) weights for the beneficial outcomes; must add up to 1
-  # weightsHarm = (vector of) weights for the harmful outcomes; must add up to 1
-  # labelsBen = string vector of outcome labels for the beneficial outcomes
-  # labelsHArm = string vector of outcome labels for the harmful outcomes
-  # lambda = (vector of) threshold values needed for the calculation of net benefit; values should be between 0 and 1
+  # outcomesBen = (vector of) of benefits outcomes measures with treatments as row names; must be between 0-1
+  # outcomesHarm = (vector of) of harms/acceptability outcomes measures with treatments as row names; must be between 0-1
+  # weightsBen = (vector of) weights for the benefits outcomes, same length as outcomesBen; must add up to 1
+  # weightsHarm = (vector of) weights for the harms/acceptability outcomes, same length as outcomesHarm; must add up to 1
+  # labelsBen = string vector of outcome labels for the benefits outcomes,  same length as outcomesBen
+  # labelsHArm = string vector of outcome labels for the harms/acceptability outcomes, same length as outcomesHarm
+  # lambda = (vector of) trade-off values needed for the calculation of net benefit; values should be between 0 and 1
   
+  
+  
+  # calculation of standardised area within the benefits spie chart for each treatment (SAWIS_B)
   pos <- t(sapply(rownames(outcomesBen) , function(i) spie.chart.B(as.numeric(outcomesBen[i,]),theta=weightsBen*2*pi,outcome.label = labelsBen)))
   colnames(pos) <- c("chart", "area")
   
+  # calculation of standardised area within the harms spie chart for each treatment (SAWIS_H)
   neg <- t(sapply(rownames(outcomesHarm) , function(i) spie.chart.H(as.numeric(outcomesHarm[i,]),theta=weightsHarm*2*pi,outcome.label = labelsHarm)))
   colnames(neg) <- c("chart", "area")
   
 
+  # calculation of net-benefit standardised area within the spie chart for each treatment (SAWIS_NB)
   NB <- array(data = NA, dim = c(length(lambda), nrow(outcomesBen)), dimnames = list(lambda, rownames(outcomesBen)))
   for (i in 1:length(lambda)) {
     NB[i,] <- as.numeric(gsub("Area inside spie chart = ", "", pos[,"area"])) - (lambda[i]*as.numeric(gsub("Area inside spie chart = ", "", neg[,"area"])))
@@ -26,3 +31,5 @@ NBspie <- function(outcomesBen, outcomesHarm, weightsBen, weightsHarm, labelsBen
 }
 
 
+# to show values of SAWIS_NB for all treatment across all values of lambda run: NBspie$NetBenefit
+# this can then be used to produce plots
